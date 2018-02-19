a639ffe0883106cad26f211c4839c67872f9b238
Manan <manansoni1399@gmail.com>
2018-02-19 17:46:51 +0530

    segementation - count number of spaces

--- segementation/detect.py
+++ segementation/detect.py
@@ -65,6 +65,48 @@ if len(start)!=len(end):
 words = len(start)
 print('words:',words)
 
+#make array to store space distances
+sdist=[]
+#border case-1: touching left edge
+if start[0]!=0:
+    sdist.append(start[0])
+#general case
+for i in range(len(start)-1):
+    sdist.append(start[i+1] - end[i])
+#border case-2: touching right edge
+if end[-1]!=im.size[0]-1:
+    sdist.append(im.size[0]-1 - end[-1])
+
+#make array to store difference between consecutive spaces
+sdiff=[]
+ssort = sorted(sdist,reverse=True)
+for i in range(len(ssort)-1):
+    sdiff.append(ssort[i] - ssort[i+1])
+m = max(sdiff)
+avg = sum(sdist)/len(sdist)
+#find wall(index) for max space difference
+for i in range(len(ssort)-1):
+    if ssort[i]-ssort[i+1]==m:
+        wall = i+1
+        break
+#count number of spaces through wall
+spaces = 0
+for i in range(len(sdist)):
+    if sdist[i] > ssort[wall]:
+        spPos = i
+        spaces+=1
+if m - avg < 0.4*avg:
+    spaces = 0
+print('spaces:',spaces)
+
+#sentence formation
+sentence = []
+for i in range(words+spaces):
+    sentence.append(None)
+sentence[spPos] = ' '
+print(sentence)
+
+#save for viewing and debugging
 hor.save('hor.jpg')
 ver.save('ver.jpg')
 gray.save('gray.jpg')
