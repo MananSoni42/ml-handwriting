394030c3e4396281b3479695d4b81311c51f9b5b
Manan <manansoni1399@gmail.com>
2018-02-22 02:32:49 +0530

    segmentation - tweak space finding algorithm for better results

--- segementation/detect.py
+++ segementation/detect.py
@@ -3,9 +3,12 @@ import sys
 from PIL import Image
 import os
 
-if len(sys.argv)!=2:
-    print('Usage: ./detect.py imageName')
+#open all neccessary files
+if len(sys.argv)!=3:
+    print('Usage: ./detect.py imageName inputFile')
     sys.exit()
+im = Image.open(sys.argv[1])
+out = open(sys.argv[2],'w')
 
 def convTo28x28(img):
     width,height = img.size
@@ -35,7 +38,6 @@ def convTo28x28(img):
     return final
 
 #set up gray,hor,ver images
-im = Image.open(sys.argv[1])
 gray = im.convert("L")
 ver = Image.new('RGB',im.size,(255,255,255))
 
@@ -59,7 +61,7 @@ for i in range(1,im.size[0]-1):
 #border cases
 #case 1 - touching left edge
 if ver.getpixel((0,100))==(255,0,0):
-    vbound[0]==2
+    vbound[0]=2
 #case 2 - touching right edge
 if ver.getpixel((im.size[0]-1,100))==(255,0,0):
     vbound[im.size[0]-1]=1
@@ -71,11 +73,9 @@ for i in range(im.size[0]):
     if vbound[i]==1:
         for j in range(im.size[1]):
             ver.putpixel( (i,j) , (255,0,0) )
-#            gray.putpixel( (i,j) , (0) )
     if vbound[i]==2:
         for j in range(im.size[1]):
             ver.putpixel( (i,j) , (0,0,255) )
-#            gray.putpixel( (i,j) , (0) )
 
 #create lists to identify start and end of words
 start = []
@@ -88,57 +88,81 @@ for i in range(len(vbound)):
 del vbound
 if len(start)!=len(end):
     print('incorrect border cases (start,end)')
+    ver.show()
     sys.exit()
 
-#count number of words and print
+#count number of words
 words = len(start)
-#print('words:',words)
-
 #make array to store space distances
 sdist=[]
+fstart = False
+fend = False
 #border case-1: touching left edge
 if start[0]!=0:
     sdist.append(start[0])
+    fstart = True
 #general case
 for i in range(len(start)-1):
     sdist.append(start[i+1] - end[i])
 #border case-2: touching right edge
 if end[-1]!=im.size[0]-1:
     sdist.append(im.size[0]-1 - end[-1])
+    fend = True
 
 #make array to store difference between consecutive spaces
 sdiff=[]
 ssort = sorted(sdist,reverse=True)
 for i in range(len(ssort)-1):
     sdiff.append(ssort[i] - ssort[i+1])
-m = max(sdiff)
-avg = sum(sdist)/len(sdist)
 #find wall(index) for max space difference
+m = max(sdiff)
 for i in range(len(ssort)-1):
     if ssort[i]-ssort[i+1]==m:
         wall = i+1
         break
 #count number of spaces through wall
 spaces = 0
+spPos=[]
 for i in range(len(sdist)):
     if sdist[i] > ssort[wall]:
-        spPos = i
         spaces+=1
-if m - avg < 0.4*avg:
+
+#disregerding cases where 0 spaces should be found
+av1 = sum( [ssort[i] for i in range(wall)] ) / wall
+av2 = sum( [ssort[i] for i in range(wall,len(ssort))] ) / (len(ssort)-wall)
+av3 = sum(ssort)/len(ssort)
+if av1 - av3 < 0.75*av3:
     spaces = 0
-#print('spaces:',spaces)
 
 #sentence formation
 sentence = []
-for i in range(words+spaces):
-    sentence.append(None)
-sentence[spPos] = ' '
+if spaces == 0:
+    for i in range(len(start)):
+        sentence.append(None)
+else:
+    struct = []
+    for i in range(len(sdist)):
+        struct.append('w')
+        struct.append(sdist[i])
+    struct.append('w')
+    if fstart == True:
+        del struct[0]
+    if fend == True:
+        del struct[-1]
+    for i in range(len(struct)):
+        if struct[i]=='w':
+            sentence.append(None)
+        else:
+            if struct[i] > ssort[wall]:
+                sentence.append(' ')
+
+#write sentence structure to file
 for i in range(len(sentence)):
     if sentence[i]==None:
-        print('N',end=' ')
+        out.write('W ')
     if sentence[i]==' ':
-        print('S',end=' ')
-print('')
+        out.write('S ')
+out.write('\n')
 
 #find horizontal lines per word
 up=[]
@@ -185,25 +209,14 @@ for i in range(len(start)):
         gray.putpixel((start[i],k),0)
         gray.putpixel((end[i],k),0)
 
-#create folder crop and change dir to crop
-path = os.path.abspath('.')
-newPath = path + '/crop' + '_' + str(sys.argv[1])
-if not os.path.exists(newPath):
-    os.makedirs(newPath)
-os.chdir(newPath)
-
 inp = []
 
 #add cropped images to crop
 for i in range(len(start)):
     #add temporary images
     temp = gray.crop((start[i],up[i],end[i]+1,down[i]+1))
-    name = str(i) + '.jpg'
-    temp.save(name)
     #add final images + create input array
     new = convTo28x28(temp)
-    name = str(i) + '_f.jpg'
-    new.save(name)
     #create and add input
     inp.append([])
     for j in range(28):
@@ -212,10 +225,10 @@ for i in range(len(start)):
 
 
 #save for viewing and debugging
-os.chdir(path)
-gray.show()
+name = 'final_' + sys.argv[1]
+gray.save(name)
 #input file
 for i in range(len(inp)):
     for j in range(len(inp[i])):
-        print(inp[i][j],end=' ')
-    print('')
+        out.write(str(inp[i][j])+' ')
+    out.write('\n')
