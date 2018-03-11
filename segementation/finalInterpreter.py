f8bac8fa23591dcb9d31df072373e310480cde9d
coolsidd <sidharth_0903@hotmail.com>
2018-03-11 12:37:20 +0530

    reverted the finalInterpreter to be for single net

--- segementation/finalInterpreter.py
+++ segementation/finalInterpreter.py
@@ -2,13 +2,13 @@
 
 import shelve,os,sys,numpy
 
-if len(sys.argv) != 5:
-    print("Usage ./finalInterpreter.py inputImages sentenceStruct shelveFile shelveFile2")
+if len(sys.argv) != 4:
+    print("Usage ./finalInterpreter.py inputImages sentenceStruct shelveFile")
     sys.exit()
 images = open(sys.argv[1],"r")
 sentenceStruct= open(sys.argv[2],"r")
 IMAGESIZE = 28*28
-db = [shelve.open(str(sys.argv[3])),shelve.open(str(sys.argv[4]))]
+db = shelve.open(str(sys.argv[3]))
         
 def getImages(inFile,inpNum,inpSize):
     #extract data from txt file
@@ -75,54 +75,38 @@ for i in range(X.shape[0]):
                 print('0')
 X=X.T
 '''
-THETA1 = []
-THETA2 = []
+THETA = []
 try:
-    THETA1 = db[0]['theta']
-    THETA2 = db[1]['theta']
+    THETA = db['theta']
 except:
     print("couldn't find theta in shelve")
     sys.exit()
 
 
-NEURONS1 = [THETA1[0].shape[1]-1]
-NEURONS2 = [THETA2[0].shape[1]-1]
+NEURONS = [THETA[0].shape[1]-1]
+for i in range(len(THETA)):
+    NEURONS.append(THETA[i].shape[0])
+                 
+A = forwardPropogation(X,NEURONS,THETA)
 
-for i in range(len(THETA1)):
-    NEURONS1.append(THETA1[i].shape[0])
-for i in range(len(THETA2)):
-    NEURONS2.append(THETA2[i].shape[0])
-
-A1 = forwardPropogation(X,NEURONS1,THETA1)
-A2 = forwardPropogation(X,NEURONS2,THETA2)
-
-A1[-1] = A1[-1].T
-A2[-1] = A2[-1].T
+A[-1] = A[-1].T
 
 #print(A[-1])
-m1=[]
-m2=[]
+m=0
 finalOut = []
-for i in range(A1[-1].shape[0]):
-    m1=max(A1[-1][i])
-    m2=max(A2[-1][i])
-    m1 -= 0.01
-    m2 -= 0.01
-    if m1 > m2:
-        for j in range(A1[-1].shape[1]):
-            if A1[-1][i][j] >= m1:
-                A1[-1][i][j] = 1
-                finalOut.append(chr(j+ord('A')))
-            else:
-                A1[-1][i][j] = 0
-    else:
-        for j in range(A2[-1].shape[1]):
-            if A2[-1][i][j] >= m2:
-                A2[-1][i][j] = 1
+for i in range(A[-1].shape[0]):
+    m=max(A[-1][i])
+    m -= 0.01
+    for j in range(A[-1].shape[1]):
+        if A[-1][i][j] >= m:
+            A[-1][i][j] = 1
+            if j<10:
                 finalOut.append(chr(j+ord('0')))
             else:
-                A2[-1][i][j] = 0
-
+                finalOut.append(chr(j-10+ord('A')))
+        else:
+            A[-1][i][j] = 0
+  
 for i in range(len(lines)):
     lines[i]=list(lines[i])
 
