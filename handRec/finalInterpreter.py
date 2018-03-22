d83bf90b2689069a413ef741cdc9b660fe4f6e4e
coolsidd <sidharth_0903@hotmail.com>
2018-03-22 18:29:40 +0530

    added multiple neural nets trained on individual classes

--- handRec/finalInterpreter.py
+++ handRec/finalInterpreter.py
@@ -2,14 +2,15 @@
 
 import shelve,os,sys,numpy
 
-if len(sys.argv) != 4:
-    print("Usage ./finalInterpreter.py inputImages sentenceStruct shelveFile")
+if len(sys.argv) != 3:
+    print("Usage ./finalInterpreter.py inputImages sentenceStruct")
     sys.exit()
 images = open(sys.argv[1],"r")
 sentenceStruct= open(sys.argv[2],"r")
 IMAGESIZE = 28*28
-db = shelve.open(str(sys.argv[3]))
-
+db = shelve.open("weights")
+db2= shelve.open("weights1")
+db3= shelve.open("weights2")
 def getImages(inFile,inpNum,inpSize):
     #extract data from txt file
     data =  []
@@ -78,35 +79,49 @@ X=X.T
 THETA = []
 try:
     THETA = db['theta']
+    print("SUCCESS")
+    THETA2 = db2['theta']
+    print("SUCCESS")
+    THETA3 = db3['theta']
 except:
     print("couldn't find theta in shelve")
     sys.exit()
 
 
 NEURONS = [THETA[0].shape[1]-1]
+NEURONS2 = [THETA2[0].shape[1]-1]
+NEURONS3 = [THETA3[0].shape[1]-1]
+
 for i in range(len(THETA)):
     NEURONS.append(THETA[i].shape[0])
+    NEURONS2.append(THETA2[i].shape[0])
+    NEURONS3.append(THETA3[i].shape[0])
 
 A = forwardPropogation(X,NEURONS,THETA)
+A2 = forwardPropogation(X,NEURONS2,THETA2)
+A3 = forwardPropogation(X,NEURONS3,THETA3)
 
-A[-1] = A[-1].T
+matFinal = numpy.concatenate((A[-1]*0.8,A2[-1]*0.90,A3[-1]),axis=0)
+matFinal = matFinal.T
 
-#print(A[-1])
+print(matFinal)
 m=0
 finalOut = []
-for i in range(A[-1].shape[0]):
-    m=max(A[-1][i])
+for i in range(matFinal.shape[0]):
+    m=max(matFinal[i])
     m -= 2**(-5)
-    for j in range(A[-1].shape[1]):
-        if A[-1][i][j] >= m:
-            A[-1][i][j] = 1
+    for j in range(matFinal.shape[1]):
+        if matFinal[i][j] >= m:
+            matFinal[i][j] = 1
             if j<10:
                 finalOut.append(chr(j+ord('0')))
-            else:
+            elif j<36:
                 finalOut.append(chr(j-10+ord('A')))
+            else:
+                finalOut.append(chr(j-36+ord('a')))
             break
         else:
-            A[-1][i][j] = 0
+            matFinal[i][j] = 0
 
 for i in range(len(lines)):
     lines[i]=list(lines[i])
